26ec84686b15e2ccad032e7df71883fd1ba96d94
Finn Biggs <finn@auli.tech>
2024-06-20 14:33:52 -0700

    Error Recreated

---
+++ build_dk/image_preload.cmake
@@ -0,0 +1,11 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/users/finnb/documents/auli/mcuboot_exfat;C:/ncs/v2.5.0/nrf;C:/ncs/v2.5.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/Users/finnb/Documents/auli/MCUBoot_ExFat/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "c:/Users/finnb/Documents/auli/MCUBoot_ExFat/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
+set(mcuboot_DTC_OVERLAY_FILE "C:/ncs/v2.5.0/nrf/modules/mcuboot/usb.overlay" CACHE INTERNAL "NCS child image controlled")
+set(mcuboot_DTC_OVERLAY_FILE "C:/ncs/v2.5.0/nrf/modules/mcuboot/usb.overlay" CACHE INTERNAL "NCS child image controlled")
+set(mcuboot_OVERLAY_CONFIG "C:/ncs/v2.5.0/nrf/subsys/partition_manager/partition_manager_enabled.conf" CACHE INTERNAL "NCS child image controlled")
---
+++ build_dk/mcuboot/shared_vars.cmake
@@ -0,0 +1,8 @@
+add_custom_target(mcuboot_shared_property_target)
+set_property(TARGET mcuboot_shared_property_target  PROPERTY KERNEL_HEX_NAME;zephyr.hex)
+set_property(TARGET mcuboot_shared_property_target  PROPERTY ZEPHYR_BINARY_DIR;C:/Users/finnb/Documents/auli/MCUBoot_ExFat/build_dk/mcuboot/zephyr)
+set_property(TARGET mcuboot_shared_property_target  PROPERTY KERNEL_ELF_NAME;zephyr.elf)
+set_property(TARGET mcuboot_shared_property_target  PROPERTY BUILD_BYPRODUCTS;C:/Users/finnb/Documents/auli/MCUBoot_ExFat/build_dk/mcuboot/zephyr/zephyr.hex;C:/Users/finnb/Documents/auli/MCUBoot_ExFat/build_dk/mcuboot/zephyr/zephyr.elf)
+set_property(TARGET mcuboot_shared_property_target  PROPERTY SIGNATURE_KEY_FILE;root-ec-p256.pem)
+set_property(TARGET mcuboot_shared_property_target APPEND PROPERTY PM_YML_DEP_FILES;C:/ncs/v2.5.0/bootloader/mcuboot/boot/zephyr/pm.yml)
+set_property(TARGET mcuboot_shared_property_target APPEND PROPERTY PM_YML_FILES;C:/Users/finnb/Documents/auli/MCUBoot_ExFat/build_dk/mcuboot/zephyr/include/generated/pm.yml)
